15c219e6813d7dfb846fb0301dc581f59f4d7363
David-Sarif <DavidSarif@yandex.ru>
2020-04-14 03:47:52 +0300

    feat:moved cards data into separate module

--- src/module.js
+++ src/module.js
@@ -1,2 +1,68 @@
-const hi = 'hello!';
-export default hi;
+const cardsArray = [
+  {
+    name: 'lynx',
+    img: './src/cards/cats/lynx.jpg',
+    nameRus: 'Рысь',
+    nameEng: 'Lynx',
+    audio: '',
+    category: 'cats',
+  },
+  {
+    name: 'panther',
+    img: './src/cards/cats/panther.jpg',
+    nameRus: 'Пантера',
+    nameEng: 'Panther',
+    audio: '',
+    category: 'cats',
+  },
+  {
+    name: 'snow_leopard',
+    img: './src/cards/cats/snow_leopard.jpg',
+    nameRus: 'Снежный Барс',
+    nameEng: 'Snow Leopard',
+    audio: '',
+    category: 'cats',
+  },
+  {
+    name: 'leopard',
+    img: './src/cards/cats/leopard.jpg',
+    nameRus: 'Леопард',
+    nameEng: 'Leopard',
+    audio: '',
+    category: 'cats',
+  },
+  {
+    name: 'lion',
+    img: './src/cards/cats/lion.jpg',
+    nameRus: 'Лев',
+    nameEng: 'Lion',
+    audio: '',
+    category: 'cats',
+  },
+  {
+    name: 'cheetah',
+    img: './src/cards/cats/cheetah.jpg',
+    nameRus: 'Гепард',
+    nameEng: 'Cheetah',
+    audio: '',
+    category: 'cats',
+  },
+  {
+    name: 'manul',
+    img: './src/cards/cats/manul.jpg',
+    nameRus: 'Манул',
+    nameEng: 'Manul',
+    audio: '',
+    category: 'cats',
+  },
+  {
+    name: 'tiger',
+    img: './src/cards/cats/tiger.jpg',
+    nameRus: 'Тигр',
+    nameEng: 'Tiger',
+    audio: '',
+    category: 'cats',
+  },
+];
+
+export default cardsArray;
