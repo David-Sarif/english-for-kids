ef930b9db3a71e1f2308485be1f042a496b2fe7d
David-Sarif <DavidSarif@yandex.ru>
2020-04-15 05:39:03 +0300

    feat: navbar is now procedural generated

--- english-for-kids/src/script.js
+++ english-for-kids/src/script.js
@@ -2,6 +2,10 @@ import cardsArray from './cards.js';
 
 const cardsContainer = document.getElementById('cards-container');
 const nav = document.getElementById('nav');
+const toggleNavLabel = document.getElementById('toggle-nav-label');
+const toggleNav = document.getElementById('toggle-nav');
+const navList = document.getElementById('nav-list')
+
 const drawCards = function (cards) {
   cardsContainer.innerHTML = '';
   // eslint-disable-next-line no-restricted-syntax
@@ -24,6 +28,20 @@ const drawCards = function (cards) {
 };
 drawCards(cardsArray.filter((card) => card.category === 'categories'));
 
+const generateNav = function (){
+  const categories = cardsArray.filter((card) => card.category === 'categories');
+  for (const elem of categories){
+    const category = document.createElement('li');
+    category.classList.add('nav-element');
+    category.id = elem.name;
+    category.innerHTML = `<img src="${elem.img}" class='nav-element__img' alt="">${elem.name}`;
+    navList.append(category);
+  }
+  
+}
+
+generateNav()
+
 cardsContainer.addEventListener('click', (event) => {
   if (event.target.closest('card')) {
     const chosenCard = event.target.closest('card').id;
@@ -40,12 +58,13 @@ nav.addEventListener('click', (event) =>{
     const chosenCard = event.target.closest('li').id;
     
     drawCards(cardsArray.filter((card) => card.category === chosenCard));
-    nav.classList.add('visually-hidden')
+    nav.classList.add('visually-hidden');
+    toggleNav.checked = false;
 
   }
   
 })
 
-document.querySelector("body > header > div > div.hamburger-toggle > label").addEventListener('click', (event) =>{
+toggleNavLabel.addEventListener('click', (event) =>{
   nav.classList.toggle('visually-hidden')
 })
